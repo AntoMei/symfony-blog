c63f0d7b70d999329038c6fa1b0581c60f2633e4
AntoMei <antonio.meirosu444@gmail.com>
2022-10-26 10:34:38 +0200

    Finalizado los primero pasos

---
+++ symfony-blog/src/Controller/BlogController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+    * @Route("/blog", name="blog")
+    */
+    public function blog(): Response
+    {
+        return $this->render('blog/blog.html.twig', []);
+    }
+    /**
+    * @Route("/single_post", name="single_post")
+    */
+    public function single_post(): Response
+    {
+        return $this->render('blog/blog.html.twig', []);
+    }
+}
--- symfony-blog/src/Controller/PageController.php
+++ symfony-blog/src/Controller/PageController.php
@@ -20,4 +20,11 @@ class PageController extends AbstractController
     {
     return $this->render('page/about.html.twig', []);
     }
+    /**
+    * @Route("/contact", name="contact")
+    */
+    public function contact(): Response
+    {
+    return $this->render('page/contact.html.twig', []);
+    }
 }
